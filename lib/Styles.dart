66972f0a4bf230e123abf41aa7c71c986bd2d204
GurjapSingh <sjapgurjap@gmail.com>
2019-06-29 13:15:35 +0530

    init

---
+++ lib/Styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Style{
+  static const boldTextStyle=TextStyle(fontWeight: FontWeight.w700);
+  static const italicTextStyle=TextStyle(fontStyle: FontStyle.italic);
+  static const normalTextStyle=TextStyle();
+  static const underLineTextStyle=TextStyle(decoration: TextDecoration.underline,);
+  static const lineThroughTextStyle=TextStyle(decoration: TextDecoration.lineThrough);
+
+}
--- lib/draft_js_to_textspan_flutter.dart
+++ lib/draft_js_to_textspan_flutter.dart
@@ -1,13 +1,31 @@
 library draft_js_to_textspan_flutter;
-import 'package:flutter/material.dart';
-class DraftJSFlutter extends StatelessWidget{
-  final String draftJsJsonString;
 
-  DraftJSFlutter(this.draftJsJsonString);
+import 'package:draft_js_to_textspan_flutter/model.dart';
+import 'package:flutter/material.dart';
+
+class DraftJSFlutter extends StatelessWidget {
+  final String draftJsJsonString;
+  final Map<String, dynamic> map;
+
+  DraftJSFlutter(this.draftJsJsonString, this.map);
+
+  List<TextSpan> getTextSpans() {
+    List<TextSpan> list = List();
+    if (map != null) {
+      DraftJsObject draftJsObject = DraftJsObject.fromJson(map);
+      if (draftJsObject != null && draftJsObject.blocks != null) {
+        for (int blockIndex = 0;
+            blockIndex < draftJsObject.blocks.length;
+            blockIndex++) {
+          list.add(TextSpan(text: draftJsObject.blocks[blockIndex].text ?? ""));
+        }
+      }
+    }
+    return list;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text("Hello world!");
+    return RichText(text: TextSpan(children: []..addAll(getTextSpans())));
   }
-
 }
