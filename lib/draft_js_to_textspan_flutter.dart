519fe5ba22b7108108677681cddcd510d5a3a29d
GurjapSingh <sjapgurjap@gmail.com>
2019-06-29 12:54:18 +0530

    init

--- lib/draft_js_to_textspan_flutter.dart
+++ lib/draft_js_to_textspan_flutter.dart
@@ -7,16 +7,7 @@ class DraftJSFlutter extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return RichText(
-      text: TextSpan(
-        text: 'Hello ',
-        style: DefaultTextStyle.of(context).style,
-        children: <TextSpan>[
-          TextSpan(text: 'bold', style: TextStyle(fontWeight: FontWeight.bold)),
-          TextSpan(text: ' world!'),
-        ],
-      ),
-    );
+    return Text("Hello world!");
   }
 
 }
