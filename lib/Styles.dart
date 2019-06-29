2f48a4e8674273381d9411939e971ac480f001eb
GurjapSingh <sjapgurjap@gmail.com>
2019-06-29 13:25:15 +0530

    init

--- lib/Styles.dart
+++ lib/Styles.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 class Style{
-  static const boldTextStyle=TextStyle(fontWeight: FontWeight.w700);
-  static const italicTextStyle=TextStyle(fontStyle: FontStyle.italic);
-  static const normalTextStyle=TextStyle();
-  static const underLineTextStyle=TextStyle(decoration: TextDecoration.underline,);
-  static const lineThroughTextStyle=TextStyle(decoration: TextDecoration.lineThrough);
+  static const boldTextStyle=TextStyle(color:Colors.black,fontWeight: FontWeight.w700);
+  static const italicTextStyle=TextStyle(color:Colors.black,fontStyle: FontStyle.italic);
+  static const normalTextStyle=TextStyle(color:Colors.black,);
+  static const underLineTextStyle=TextStyle(color:Colors.black,decoration: TextDecoration.underline,);
+  static const lineThroughTextStyle=TextStyle(color:Colors.black,decoration: TextDecoration.lineThrough);
 
 }
